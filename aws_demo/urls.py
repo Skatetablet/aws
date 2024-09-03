993b17f81af7be333fe493cf505cb954d895638c
Skatetablet <skatewer@gmail.com>
2024-09-02 17:56:32 -0700

    first commit

---
+++ aws_demo/urls.py
@@ -0,0 +1,10 @@
+from rest_framework import routers
+
+from .views import TaskViewSet
+
+router = routers.DefaultRouter()
+
+router.register("tasks", TaskViewSet, "tasks")
+
+
+urlpatterns = router.urls
